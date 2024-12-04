ee5db6777bc322915707fd68f96488741ee0d5ed
Le Luong <thienle.luong@gmail.com>
2024-12-04 20:23:03 +0700

    update env

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-config({ path: ".env.local" }); // or .env.local
+config({ path: ".env" }); // or .env.local
 
 
 
