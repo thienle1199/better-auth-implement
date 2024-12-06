447fc98ba7e36b2163bd11fb332622f083f89ed1
Le Luong <thienle.luong@gmail.com>
2024-12-06 10:12:37 +0700

    add google login

--- src/app/signin/components/SignInForm.tsx
+++ src/app/signin/components/SignInForm.tsx
@@ -1,89 +1,100 @@
-"use client"
+"use client";
 import { authClient } from "@/lib/auth-client"; //import the auth client
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import Link from "next/link";
- 
-export default function SignInForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
- 
-  const signIn = async (evt: FormEvent) => {
-    evt.preventDefault();
-    await authClient.signIn.email({ 
-        email, 
-        password,
-     }, { 
-        onRequest: () => { 
-         setIsLoading(true);
-        }, 
-        onSuccess: () => { 
-          router.push("/");
-        }, 
-        onError: (ctx) => { 
-          setError(ctx.error.message)
-          setIsLoading(false);
-        }, 
-      }); 
-  };
- 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
-          <CardDescription>Enter your credentials to access your account</CardDescription>
-        </CardHeader>
-        <form onSubmit={signIn}>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="name@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
 
-            <div className="text-sm text-right">
-                <Link href="/reset-password" className="text-blue-600 hover:underline">
-                  Forgot password?
-                </Link>
-              </div>
-            {error && (
-              <Alert variant="destructive">
-                <AlertDescription>{error}</AlertDescription>
-              </Alert>
-            )}
-          </CardContent>
-          <CardFooter>
-            <Button type="submit" className="w-full" disabled={isLoading}>
-              {isLoading ? 'Logging in...' : 'Log in'}
-            </Button>
-          </CardFooter>
-        </form>
-      </Card>
-    </div>
-  )
+export default function SignInForm() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const router = useRouter();
+
+	const signIn = async (evt: FormEvent) => {
+		evt.preventDefault();
+		await authClient.signIn.email(
+			{
+				email,
+				password,
+			},
+			{
+				onRequest: () => {
+					setIsLoading(true);
+				},
+				onSuccess: () => {
+					router.push("/");
+				},
+				onError: (ctx) => {
+					setError(ctx.error.message);
+					setIsLoading(false);
+				},
+			}
+		);
+	};
+
+  const signInWithGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/",
+    }, {
+      onError: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<Card className="w-full max-w-md">
+				<CardHeader>
+					<CardTitle className="text-2xl font-bold">Login</CardTitle>
+					<CardDescription>Enter your credentials to access your account</CardDescription>
+				</CardHeader>
+				<form onSubmit={signIn}>
+					<CardContent className="space-y-4">
+						<div className="space-y-2">
+							<Label htmlFor="email">Email</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder="name@example.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+						<div className="space-y-2">
+							<Label htmlFor="password">Password</Label>
+							<Input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+						</div>
+
+						<div className="text-sm text-right">
+							<Link href="/reset-password" className="text-blue-600 hover:underline">
+								Forgot password?
+							</Link>
+						</div>
+						{error && (
+							<Alert variant="destructive">
+								<AlertDescription>{error}</AlertDescription>
+							</Alert>
+						)}
+					</CardContent>
+					<CardFooter>
+						<Button type="submit" className="w-full" disabled={isLoading}>
+							{isLoading ? "Logging in..." : "Log in"}
+						</Button>
+					</CardFooter>
+				</form>
+				<Button onClick={signInWithGoogle} variant="outline" className="w-full">
+					Log in with Google
+				</Button>
+			</Card>
+		</div>
+	);
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,12 +3,20 @@ import * as schema from "@/db/schema";
 import { sendEmail } from "@/lib/send-email";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
- 
+import { nextCookies } from "better-auth/next-js";
+
 export const auth = betterAuth({
+    plugins: [nextCookies()],
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema,
     }),
+    socialProviders: {
+      google: {
+          clientId: process.env.GOOGLE_CLIENT_ID!,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }
+    },
     trustedOrigins: [`${process.env.BETTER_AUTH_URL}/signup`, `${process.env.BETTER_AUTH_URL}/signin`, `${process.env.BETTER_AUTH_URL}`],
     
     emailAndPassword: {
